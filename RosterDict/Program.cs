0faa0f70aaf22ff08ced21a96223583e2e06dec3
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-02-27 21:53:27 -0600

    Adds Class 2 prep: RosterDict.

---
+++ RosterDict/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosterDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.Write("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    // Get the student's ID Number
+                    Console.Write("ID Number: ");
+                    int newID = int.Parse(Console.ReadLine());
+
+                    students.Add(newID, newStudent);
+                }
+            }
+            while (newStudent != "");
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Value + " (" + student.Key + ")");
+            }
+            Console.ReadLine();
+        }
+    }
+}
