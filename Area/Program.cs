b838d97498a000f408a5b5c0499d5b352b2764f9
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-02-27 19:38:03 -0600

    Adds studio 1, "Area of a Circle".

---
+++ Area/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a radius: ");
+            string input = Console.ReadLine();
+            float radius;
+            bool isNumber = float.TryParse(input, out radius);
+            while (isNumber == false || radius < 0)
+            {
+                Console.Write("Enter a positive number for the radius: ");
+                input = Console.ReadLine();
+                isNumber = float.TryParse(input, out radius);
+            }
+            Console.WriteLine("The area of a circle with a radius of " + radius + " is: " + (radius * radius * 3.14));
+            Console.ReadLine();
+        }
+    }
+}
