c355008dc6ff60c5fbd2e88be2241d2eb9cfb4ee
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-02-27 20:19:15 -0600

    Adds Class 2 prep: SumEvens.

---
+++ SumEvens/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            List<int> nums = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine("The sum of even numbers in the list is " + sum +".");
+            Console.ReadLine();
+        }
+    }
+}
