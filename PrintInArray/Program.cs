a7ef39f4e3338b24d0617f24b1648b521cfa9851
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-02-27 21:47:24 -0600

    Adds Class 2 prep: PrintInArray.

---
+++ PrintInArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in myArray)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
