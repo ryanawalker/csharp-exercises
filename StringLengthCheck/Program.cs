797b380564e3b7be00edd40ee94e7bff7cabd222
Ryan A. Walker <rawn8c@mail.umsl.edu>
2017-02-27 21:41:46 -0600

    Adds Class 2 prep: StringLengthCheck.

---
+++ StringLengthCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLengthCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string> {"12345", "1234", "56789", "434", "23245566", "lists", "chars", "five"};
+            foreach (string item in myList)
+            {
+                if (item.Length == 5)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
